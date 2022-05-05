5da220b633a8692638aef553b0e1e5d622e03891
Niklas <niklaslehle@gmail.com>
2022-05-05 17:05:46 +0200

    Add InspectorProxy to depricate makeBot

--- src/index.ts
+++ src/index.ts
@@ -23,114 +23,145 @@ interface ProxyInspectorEmitter extends EventEmitter {
   on(event: 'proxyBotTookControl', listener: () => void): this
 }
 
-export function makeBot(options: BotOptions, proxyOptions?: ProxyOptions) {
-  const conn = new Conn(options)
-  proxyOptions = proxyOptions ?? {}
+export interface InspectorProxy {
+  on(event: 'clientConnect', listener: (client: Client) => void): this
+  on(event: 'clientDisconnect', listener: (client: Client) => void): this
+  on(event: 'clientChat', listener: (client: Client, message: string) => void): this
+  on(event: 'clientChatRaw', listener: (client: Client, message: string) => void): this
+}
 
-  const blockedPackets = ['abilities', 'position']
-  let gotPosition = false
-  let fakePlayer: FakePlayer
-  let fakeSpectator: FakeSpectator
+export class InspectorProxy extends EventEmitter {
+  options: BotOptions
+  proxyOptions: ProxyOptions
+  conn: Conn
 
-  conn.bot.proxy = {
-    botIsControlling: true,
-    emitter: new EventEmitter()
+  constructor(options: BotOptions, proxyOptions: ProxyOptions = {}) {
+    super()
+    this.options = options
+    this.proxyOptions = proxyOptions
+    this.conn = new Conn(options)
+    this.init()
   }
 
-  conn.bot.once('login', () => {
-    const server = createServer({
-      motd: proxyOptions?.motd ?? 'mc proxy bot inspector',
-      'online-mode': false,
-      port: proxyOptions?.port ?? 25566,
-      version: '1.12.2'
-    })
+  private init() {
+    const blockedPackets = ['abilities', 'position']
+    let gotPosition = false
+    let fakePlayer: FakePlayer
+    let fakeSpectator: FakeSpectator
 
-    conn.bot.once('end', () => {
-      server.close()
-    })
-  
-    server.on('login', (client) => {
-      fakePlayer = new FakePlayer(conn.bot, client)
-      fakeSpectator = new FakeSpectator(client)
-      conn.sendPackets(client as unknown as Client)
-  
-      fakePlayer.spawn()
-      fakeSpectator.makeSpectator()
-      
-      conn.attach(client as unknown as Client, {
-        toClientMiddleware: [inspector_toClientMiddleware, inspector_toClientMiddlewareRecipesFix],
-        toServerMiddleware: [inspector_toServerMiddleware]
-      })
-
-      client.on('end', () => {
-        fakePlayer.destroy()
-      })
-    })
-  })
-
-  const inspector_toServerMiddleware: PacketMiddleware = (info, pclient, data, canceler, update) => {
-    if (info.meta.name !== 'chat') return
-    if (info.meta.name === 'chat') {
-      console.info('Client chat')
-      if ((data.message as string).startsWith('$')) { // command
-        const cmd = (data.message as string).trim().substring(1) // remove $
-        if (cmd === 'link') { // link command, replace the bot on the server
-          conn.link(pclient)
-          conn.bot.proxy.botIsControlling = false
-          fakePlayer.deSpawn()
-          fakeSpectator.revertToNormal(conn.bot)
-          canceler()
-          setTimeout(() => {
-            conn.bot.proxy.emitter.emit('proxyBotLostControl')
-          })
-          return
-        } else if (cmd === 'unlink') { // unlink command, give control back to the bot
-          conn.unlink()
-          conn.bot.proxy.botIsControlling = true
-          fakePlayer.spawn()
-          fakeSpectator.makeSpectator()
-          canceler()
-          setTimeout(() => {
-            conn.bot.proxy.emitter.emit('proxyBotTookControl')
-          })
-          return
-        }
-      } else { // Normal chat messages
-        console.info('None command parse through:' + data.message) 
-        update()
-        canceler(true)
-      }
-      return
+    this.conn.bot.proxy = {
+      botIsControlling: true,
+      emitter: new EventEmitter()
     }
-  }
 
-  const inspector_toClientMiddleware: PacketMiddleware = (info, pclient, data, canceler, update) => {
-    if (canceler.isCanceled) return
-    if (info.bound !== 'client') return
-    if (blockedPackets.includes(info.meta.name)) return canceler()
-    if (info.meta.name === 'position' && data) {
-      if (!gotPosition) {
-        gotPosition = true
+    this.conn.bot.once('login', () => {
+      const server = createServer({
+        motd: this.proxyOptions.motd ?? 'mc proxy bot inspector',
+        'online-mode': false,
+        port: this.proxyOptions.port ?? 25566,
+        version: '1.12.2'
+      })
+
+      this.conn.bot.once('end', () => {
+        server.close()
+      })
+    
+      server.on('login', (client) => {
+        fakePlayer = new FakePlayer(this.conn.bot, client)
+        fakeSpectator = new FakeSpectator(client)
+        this.conn.sendPackets(client as unknown as Client)
+    
+        fakePlayer.spawn()
+        fakeSpectator.makeSpectator()
+        
+        this.conn.attach(client as unknown as Client, {
+          toClientMiddleware: [inspector_toClientMiddleware, inspector_toClientMiddlewareRecipesFix],
+          toServerMiddleware: [inspector_toServerMiddleware]
+        })
+
+        client.on('end', () => {
+          fakePlayer.destroy()
+          this.emit('clientDisconnect', client)
+        })
+
+        this.emit('clientConnect', client)
+      })
+    })
+
+    const inspector_toServerMiddleware: PacketMiddleware = (info, pclient, data, canceler, update) => {
+      if (info.meta.name !== 'chat') return
+      if (info.meta.name === 'chat') {
+        console.info('Client chat')
+        if ((data.message as string).startsWith('$')) { // command
+          const cmd = (data.message as string).trim().substring(1) // remove $
+          if (cmd === 'link') { // link command, replace the bot on the server
+            this.conn.link(pclient)
+            this.conn.bot.proxy.botIsControlling = false
+            fakePlayer.deSpawn()
+            fakeSpectator.revertToNormal(this.conn.bot)
+            canceler()
+            setTimeout(() => {
+              this.conn.bot.proxy.emitter.emit('proxyBotLostControl')
+            })
+            return
+          } else if (cmd === 'unlink') { // unlink command, give control back to the bot
+            this.conn.unlink()
+            this.conn.bot.proxy.botIsControlling = true
+            fakePlayer.spawn()
+            fakeSpectator.makeSpectator()
+            canceler()
+            setTimeout(() => {
+              this.conn.bot.proxy.emitter.emit('proxyBotTookControl')
+            })
+            return
+          }
+        } else { // Normal chat messages
+          console.info('None command parse through:' + data.message) 
+          update()
+          canceler(true)
+        }
         return
       }
-      canceler()
-      return
-    } else if (info.meta.name === 'collect') {
-      if (data.collectorEntityId === conn.bot.entity.id) {
-        data.collectorEntityId = FakePlayer.fakePlayerId
-        update()
+    }
+
+    const inspector_toClientMiddleware: PacketMiddleware = (info, pclient, data, canceler, update) => {
+      if (canceler.isCanceled) return
+      if (info.bound !== 'client') return
+      if (blockedPackets.includes(info.meta.name)) return canceler()
+      if (info.meta.name === 'position' && data) {
+        if (!gotPosition) {
+          gotPosition = true
+          return
+        }
+        canceler()
+        return
+      } else if (info.meta.name === 'collect') {
+        if (data.collectorEntityId === this.conn.bot.entity.id) {
+          data.collectorEntityId = FakePlayer.fakePlayerId
+          update()
+        }
+      }
+    }
+
+    const inspector_toClientMiddlewareRecipesFix: PacketMiddleware = (info, pclient, data, canceler) => {
+      if (canceler.isCanceled) return
+      if (info.bound !== 'client') return
+      if (info.meta.name === 'unlock_recipes') {
+        canceler()
+        return
       }
     }
   }
+}
 
-  const inspector_toClientMiddlewareRecipesFix: PacketMiddleware = (info, pclient, data, canceler) => {
-    if (canceler.isCanceled) return
-    if (info.bound !== 'client') return
-    if (info.meta.name === 'unlock_recipes') {
-      canceler()
-      return
-    }
-  }
-
-  return conn
+/**
+ * 
+ * @deprecated Use Proxy class instead
+ * @param options Proxy options
+ * @param proxyOptions 
+ * @returns 
+ */
+export function makeBot(options: BotOptions, proxyOptions?: ProxyOptions): Conn {
+  const cls = new InspectorProxy(options, proxyOptions)
+  return cls.conn
 }
