a2612a2271f27313be2f957b154996ed8544ae7f
Niklas <niklaslehle@gmail.com>
2022-05-12 00:01:18 +0200

    Update example

--- example/simpleProxy.js
+++ example/simpleProxy.js
@@ -1,10 +1,12 @@
-const { makeBot } = require('../')
+const { InspectorProxy } = require('../')
 
-const conn = makeBot({
+const proxy = new InspectorProxy({
   host: 'localhost',
   username: 'proxyBot',
   version: '1.12.2'
 })
+
+const conn = proxy.conn
 
 conn.bot.on('spawn', () => {
   console.info('Bot spawned')
